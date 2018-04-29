94206bd91444a89d985e774caf97c3a27cdd137e
Rachael <rachael.ellen@btopenworld.com>
2018-04-29 17:35:20 +0100

    added setters for adding a geoexcursion, adding a counter, and adding an excursion to completed

--- app/src/main/java/com/example/rachael/geo_tracker/GeoTracker.java
+++ app/src/main/java/com/example/rachael/geo_tracker/GeoTracker.java
@@ -5,18 +5,41 @@ import java.util.ArrayList;
 public class GeoTracker {
 
     private ArrayList<GeoExcursion> geoExcursions;
+    private ArrayList<GeoExcursion> completedGeoExcursions;
     private int counter;
 
     public GeoTracker(ArrayList<GeoExcursion> geoExcursions, int counter){
         this.geoExcursions = new ArrayList<>(geoExcursions);
         this.counter = counter;
+        this.completedGeoExcursions = new ArrayList<>();
     }
 
     public ArrayList<GeoExcursion> getGeoExcursions() {
         return geoExcursions;
     }
 
+    public void addGeoExcursions(GeoExcursion geoExcursionToAdd) {
+        geoExcursions.add(geoExcursionToAdd);
+    }
+
     public int getCounter() {
         return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    public ArrayList<GeoExcursion> getCompletedGeoExcursions() {
+        return completedGeoExcursions;
+    }
+
+    public void addToCompletedGeoExcursions(GeoExcursion geoExcursion){
+        completedGeoExcursions.add(geoExcursion);
+        counter += 1;
+    }
+
+    public void removeCompletedGeoExcursion(GeoExcursion geoExcursion){
+        completedGeoExcursions.remove(geoExcursion);
     }
 }
--- app/src/test/java/com/example/rachael/geo_tracker/GeoExcursionTest.java
+++ app/src/test/java/com/example/rachael/geo_tracker/GeoExcursionTest.java
@@ -26,7 +26,8 @@ public class GeoExcursionTest {
     ActivityType activityType1;
 
     GeoExcursion geoExcursion1;
-    ArrayList<GeoExcursion> geoExcursionList;
+    GeoExcursion geoExcursion2;
+    ArrayList<GeoExcursion> geoExcursionList1;
 
     GeoTracker geoTracker1;
 
@@ -55,10 +56,11 @@ public class GeoExcursionTest {
         activityType1 = ActivityType.WALK;
 
         geoExcursion1 = new GeoExcursion("The Fossil Tree", geoInfoList, wayPointList, activityType1);
-        geoExcursionList = new ArrayList<>();
-        geoExcursionList.add(geoExcursion1);
+        geoExcursion2 = new GeoExcursion("The Carsaig Arches", geoInfoList, wayPointList, activityType1);
+        geoExcursionList1 = new ArrayList<>();
+        geoExcursionList1.add(geoExcursion1);
 
-        geoTracker1 = new GeoTracker(geoExcursionList, 0);
+        geoTracker1 = new GeoTracker(geoExcursionList1, 0);
 
 
 
@@ -96,11 +98,36 @@ public class GeoExcursionTest {
     }
 
     @Test
+    public void canSetCounter(){
+        geoTracker1.setCounter(50);
+        assertEquals(50, geoTracker1.getCounter());
+    }
+
+    @Test
     public void geoTrackerHasExcursion(){
         assertEquals(1, geoTracker1.getGeoExcursions().size());
     }
 
+    @Test
+    public void geoTrackerCanAddExcursion(){
+        geoTracker1.addGeoExcursions(geoExcursion2);
+        assertEquals(2, geoTracker1.getGeoExcursions().size());
+    }
 
+    @Test
+    public void canAddToCompletedGeoExcursionInGeoTracker(){
+        geoTracker1.addToCompletedGeoExcursions(geoExcursion1);
+        geoTracker1.addToCompletedGeoExcursions(geoExcursion2);
+        assertEquals(2, geoTracker1.getCompletedGeoExcursions().size());
+        assertEquals(2, geoTracker1.getCounter());
+    }
+
+    @Test
+    public void canRemoveAnExcursionFromCompletedGeoExcursionsInGeoTracker(){
+        geoTracker1.addToCompletedGeoExcursions(geoExcursion1);
+        geoTracker1.removeCompletedGeoExcursion(geoExcursion1);
+        assertEquals(0, geoTracker1.getCompletedGeoExcursions().size());
+    }
 
 
 }
