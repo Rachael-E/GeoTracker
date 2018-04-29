e336b33633a2da8f57fadcb4945c93edca82407b
Rachael <rachael.ellen@btopenworld.com>
2018-04-29 14:47:34 +0100

    added RockType enum for rock type descriptions

--- app/src/main/java/com/example/rachael/geo_tracker/RockType.java
+++ app/src/main/java/com/example/rachael/geo_tracker/RockType.java
@@ -1,4 +1,24 @@
 package com.example.rachael.geo_tracker;
 
 public enum RockType {
+
+    BASALT("Basalt", "A fine grained, dense, mafic extrusive igneous rock composed of crystals of olivine, feldspar, pyroxene and Fe-Ti oxides."),
+    SANDSTONE("Sandstone", "A clastic sedimentary rock, composed of sand grade grains. Typically porous, composed of grains of quartz and feldspar."),
+    MUDSTONE("Mudstone", "A clastic sedimentary rock, composed of clay grade grains. Typically low permeability, composed predominantly of clay minerals."),;
+
+    private String rockType;
+    private String rockTypeSummary;
+
+    private RockType (String rockType, String rockTypeSummary){
+        this.rockType = rockType;
+        this.rockTypeSummary = rockTypeSummary;
+    }
+
+    public String getRockType() {
+        return rockType;
+    }
+
+    public String getRockTypeSummary() {
+        return rockTypeSummary;
+    }
 }
