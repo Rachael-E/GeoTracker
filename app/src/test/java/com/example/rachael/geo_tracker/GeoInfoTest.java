1bc8258c2a4f595a7133c5c034494cda83907a49
Rachael <rachael.ellen@btopenworld.com>
2018-04-29 15:03:04 +0100

    created tests which pass for geoinfo, including testing enums for rocktypes

--- app/src/test/java/com/example/rachael/geo_tracker/GeoInfoTest.java
+++ app/src/test/java/com/example/rachael/geo_tracker/GeoInfoTest.java
@@ -1,4 +1,46 @@
 package com.example.rachael.geo_tracker;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class GeoInfoTest {
+
+    GeoInfo geoInfo1;
+    RockType rocktype1;
+    RockType rocktype2;
+    ArrayList<RockType> rockList;
+
+    @Before
+    public void before(){
+        rocktype1 = RockType.BASALT;
+        rocktype2= RockType.SANDSTONE;
+        rockList = new ArrayList<RockType>();
+        rockList.add(rocktype1);
+        rockList.add(rocktype2);
+        geoInfo1 = new GeoInfo("This is a test for geological info", rockList);
+
+    }
+
+    @Test
+    public void geoInfoHasSummary(){
+        assertEquals("This is a test for geological info", geoInfo1.getGeoInfoSummary());
+    }
+
+    @Test
+    public void geoInfoHasRockTypes(){
+        assertEquals(2, geoInfo1.getRockTypes().size());
+    }
+
+    @Test
+    public void geoInfoRockTypeHasType(){
+        assertEquals("Basalt", geoInfo1.getRockTypes().get(0).getRockType());
+    }
+
+
+
+
 }
