1a3aea9b1be98550597097dcaa5fa22411824d3b
Rachael <rachael.ellen@btopenworld.com>
2018-04-29 14:51:54 +0100

    added GeoInfo class

--- app/src/main/java/com/example/rachael/geo_tracker/GeoInfo.java
+++ app/src/main/java/com/example/rachael/geo_tracker/GeoInfo.java
@@ -1,4 +1,22 @@
 package com.example.rachael.geo_tracker;
 
+import java.util.ArrayList;
+
 public class GeoInfo {
+
+    private String geoInfoSummary;
+    private ArrayList<RockType> rockTypes;
+
+    public GeoInfo (String geoInfoSummary, ArrayList<RockType> rockTypes){
+        this.geoInfoSummary = geoInfoSummary;
+        this.rockTypes = new ArrayList<>(rockTypes);
+    }
+
+    public String getGeoInfoSummary() {
+        return geoInfoSummary;
+    }
+
+    public ArrayList<RockType> getRockTypes() {
+        return rockTypes;
+    }
 }
