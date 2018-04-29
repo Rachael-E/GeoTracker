38a31823afdd37923f73c8c1a26fb5c9bf8fa2b9
Rachael <rachael.ellen@btopenworld.com>
2018-04-29 14:37:24 +0100

    added EraType enum for geological periods

--- app/src/main/java/com/example/rachael/geo_tracker/EraType.java
+++ app/src/main/java/com/example/rachael/geo_tracker/EraType.java
@@ -1,4 +1,28 @@
 package com.example.rachael.geo_tracker;
 
 public enum EraType {
+
+    QUATERNARY("Quaternary", 2),
+    PALAEOGENE("Palaeogene", 66),
+    CRETACEOUS("Cretaceous", 145),
+    JURASSIC("Jurassic", 201),
+    TRIASSIC("Triassic", 252),
+    PERMIAN("Permian", 299),
+    CARBONIFEROUS("Carboniferous", 359),;
+
+    private String geologyPeriod;
+    private int periodStartMYa;
+
+    private EraType(String geologyPeriod, int periodStartMYa){
+        this.geologyPeriod = geologyPeriod;
+        this.periodStartMYa = periodStartMYa;
+    }
+
+    public String getGeologyPeriod() {
+        return geologyPeriod;
+    }
+
+    public int getPeriodStartMYa() {
+        return periodStartMYa;
+    }
 }
