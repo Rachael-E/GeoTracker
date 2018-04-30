a08b81f09a487fd7347d9a840273a28e7b14b414
Rachael <rachael.ellen@btopenworld.com>
2018-04-30 16:03:47 +0100

    set up FindWalkActivity in preparation for adaptor

---
+++ app/src/main/java/com/example/rachael/geo_tracker/Activities/ExcursionSeedAdapter.java
@@ -0,0 +1,44 @@
+package com.example.rachael.geo_tracker.Activities;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.rachael.geo_tracker.Models.GeoExcursion;
+import com.example.rachael.geo_tracker.R;
+
+import java.util.ArrayList;
+
+public class ExcursionSeedAdapter extends ArrayAdapter<GeoExcursion> {
+
+    public ExcursionSeedAdapter(Context context, ArrayList<GeoExcursion> geoExcursions) {
+        super(context, 0, geoExcursions);
+    }
+
+    @Override
+    public View getView(int position, View listItemView, ViewGroup parent) {
+
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.find_excursion_items, parent, false);
+        }
+
+        GeoExcursion currentExcursion = getItem(position);
+
+        TextView excursionTitle = listItemView.findViewById(R.id.excursionTitleTextViewId);
+        excursionTitle.setText(currentExcursion.getTitle().toString());
+
+        TextView excursionType = listItemView.findViewById(R.id.excursionTypeViewId);
+        excursionType.setText(currentExcursion.getExcursionType().getActivityType().toString());
+
+        //ImageView excursionImage
+
+        return listItemView;
+    }
+
+}
+
+
+
--- app/src/main/java/com/example/rachael/geo_tracker/Activities/FindWalkActivity.java
+++ app/src/main/java/com/example/rachael/geo_tracker/Activities/FindWalkActivity.java
@@ -3,7 +3,11 @@ package com.example.rachael.geo_tracker.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.rachael.geo_tracker.Models.GeoExcursion;
 import com.example.rachael.geo_tracker.R;
+import com.example.rachael.geo_tracker.SeedFile.GeoActivitySeed;
+
+import java.util.ArrayList;
 
 public class FindWalkActivity extends AppCompatActivity {
 
@@ -11,5 +15,8 @@ public class FindWalkActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_walk);
+
+        GeoActivitySeed excursions = new GeoActivitySeed();
+        ArrayList<GeoExcursion> excursionList = excursions.getList();
     }
 }
