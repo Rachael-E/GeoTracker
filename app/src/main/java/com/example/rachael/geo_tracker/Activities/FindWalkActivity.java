c449e8e0df44e9d85098be9be70b5fd3e664d87b
Rachael <rachael.ellen@btopenworld.com>
2018-04-30 14:17:42 +0100

    added FindWalkActivity

---
+++ app/src/main/java/com/example/rachael/geo_tracker/Activities/FindWalkActivity.java
@@ -0,0 +1,15 @@
+package com.example.rachael.geo_tracker.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.rachael.geo_tracker.R;
+
+public class FindWalkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_walk);
+    }
+}
--- app/src/main/java/com/example/rachael/geo_tracker/Activities/HomeActivity.java
+++ app/src/main/java/com/example/rachael/geo_tracker/Activities/HomeActivity.java
@@ -2,14 +2,35 @@ package com.example.rachael.geo_tracker.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.rachael.geo_tracker.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    Button findWalkButton;
+    Button completedWalkButton;
+    Button newWalkButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        findWalkButton = findViewById(R.id.findAWalkButtonId);
+        completedWalkButton = findViewById(R.id.viewCompletedWalksButtonId);
+        newWalkButton = findViewById(R.id.createNewWalkButtonId);
+    }
+
+
+    public void onMakeChoiceButtonClicked(View button){
+        Log.d(getClass().toString(), "onFindWalkButton was called");
+
+        
     }
 }
+
+
+
