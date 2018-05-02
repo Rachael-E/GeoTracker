2b6f24c8d70535f223ac56a4fb970e581fcca6df
Rachael <rachael.ellen@btopenworld.com>
2018-05-02 17:27:08 +0100

    adding a menu from scratch

--- app/src/main/java/com/example/rachael/geo_tracker/Activities/HomeActivity.java
+++ app/src/main/java/com/example/rachael/geo_tracker/Activities/HomeActivity.java
@@ -5,6 +5,9 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,6 +22,24 @@ public class HomeActivity extends AppCompatActivity {
     Button completedWalkButton;
     Button newWalkButton;
     //GeoTracker geoTracker;
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_home, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.actionMenu) {
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
