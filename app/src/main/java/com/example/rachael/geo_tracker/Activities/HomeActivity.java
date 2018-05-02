c5a943f924cc85935ee19b0ecaea9fd2c63ee502
Rachael <rachael.ellen@btopenworld.com>
2018-05-02 18:11:08 +0100

    removed some menu as thought it was bugging

--- app/src/main/java/com/example/rachael/geo_tracker/Activities/HomeActivity.java
+++ app/src/main/java/com/example/rachael/geo_tracker/Activities/HomeActivity.java
@@ -1,7 +1,6 @@
 package com.example.rachael.geo_tracker.Activities;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,23 +22,25 @@ public class HomeActivity extends AppCompatActivity {
     Button newWalkButton;
     //GeoTracker geoTracker;
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.activity_home, menu);
-        return true;
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.actionMenu) {
-
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+//
+//        MenuInflater menuInflater = getMenuInflater();
+//        menuInflater.inflate(R.menu.PLACEHOLDER, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.menuHome:
+//                startActivity(new Intent(this, HomeActivity.class));
+//                return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,34 +54,33 @@ public class HomeActivity extends AppCompatActivity {
 
 //        SharedPreferencesHelper.deleteApplicationState(this);
 //      //go to SharedPref to get the existing geoTracker
-        GeoTracker geoTracker = SharedPreferencesHelper.loadApplicationState(this);
+       GeoTracker geoTracker = SharedPreferencesHelper.loadApplicationState(this);
 //        if it's null create a new GeoTracker
 
-        if(geoTracker == null){
-            geoTracker = new GeoTracker( GeoActivitySeed.seedEverything() , 0,0);
+        if (geoTracker == null) {
+            geoTracker = new GeoTracker(GeoActivitySeed.seedEverything(), 0, 0);
             SharedPreferencesHelper.saveApplicationState(this, geoTracker);
         }
 
     }
 
 
-    public void onFindExcursionButtonClicked(View button){
+    public void onFindExcursionButtonClicked(View button) {
         Log.d(getClass().toString(), "onFindWalkButton was called");
 
-        Intent intent = new Intent( this, FindWalkActivity.class);
+        Intent intent = new Intent(this, FindWalkActivity.class);
 
-//        intent.putExtra("excursions", excursions);
         startActivity(intent);
     }
 
-    public void onCompletedButtonClicked(View button){
+    public void onCompletedButtonClicked(View button) {
 
         Intent intent = new Intent(this, CompletedExcursionActivity.class);
 
         startActivity(intent);
     }
 
-    public void onNewExcursionButtonClicked(View button){
+    public void onNewExcursionButtonClicked(View button) {
 
         Intent intent = new Intent(this, NewExcursionActivity.class);
 
